b3a781631601cf61712d65cc53af5f156f84f224
thasleema <thasleema.begum520@gmail.com>
2026-06-27 11:25:09 +0530

    query with having clause added

--- SQL Queries/SQL_Queries.sql
+++ SQL Queries/SQL_Queries.sql
@@ -2,12 +2,22 @@
 SELECT * FROM ORDERS;
 
 --Retrieve order id and product name column from table orders
-SELECT ORDER_ID,PRODUCT_NAME FROM ORDERS;
+SELECT ORDER_ID, PRODUCT_NAME
+FROM ORDERS;
 
 --Retrieve data of orders who purchased laptop
 SELECT * FROM ORDERS
-WHERE PRODUCT_NAME="LAPTOP";
+WHERE PRODUCT_NAME='LAPTOP';
 
 --Retrieve product name and no of orders to that product
-SELECT PRODUCT_NAME,COUNT(*) FROM ORDERS GROUP BY PRODUCT_NAME;
+SELECT PRODUCT_NAME, COUNT(*)
+FROM ORDERS
+GROUP BY PRODUCT_NAME;
 
+--Retrieve customer names,orderid  who purchased laptops
+SELECT C.CUSTOMER_NAME, C.ORDER_ID
+FROM ORDERS S INNER JOIN CUSTOMERS C
+ON S.ORDER_ID=C.ORDER_ID;
+
+--Retrieve product name and no of sales of that product with price more than 1000
+SELECT PRODUCT_NAME,COUNT(*) FROM ORDERS GROUP BY PRODUCT_NAME HAVING PRICE>1000;
